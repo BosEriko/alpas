c1b3c08503eb4b299663bae0b216ec3f4426969c
BosEriko <bos.eriko@gmail.com>
2019-11-12 14:36:20 +0800

    remove bands v-if

--- resources/js/components/BandsComponent.vue
+++ resources/js/components/BandsComponent.vue
@@ -1,8 +1,8 @@
 <template>
-    <div v-if="bands.length">
+    <div>
         <h4 class="text-xl mb-3 mx-3 text-white pb-3 px-2 border-b border-gray-700">Bands</h4>
         <ul class="text-white mx-3 px-3">
-            <li v-for="(band, index) in bands" class="mb-2 flex">
+            <li v-for="band in bands" class="mb-2 flex">
                 <span class="mr-2"><i class="fa fa-fw fa-music" aria-hidden="true"></i></span>
                 <a :href="`/profile/${band.id}`" class="hover:underline mr-auto">{{ band.name }}</a>
                 <a :href="`/delete-band/${band.id}`" class="hover:text-red-600 smooth"><i class="fa fa-fw fa-close" aria-hidden="true"></i></a>
